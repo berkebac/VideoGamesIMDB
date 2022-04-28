4576008a139975464e93488df9857cfbec732d94
Berke Baç <berkebac@gmail.com>
2022-04-28 21:55:33 +0300

    Initial Commit

---
+++ VideoGamesIMDB/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VideoGamesIMDB
+//
+//  Created by Berke Baç on 28.04.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
